a361f55a0d0c0a0c23161751339cd145894bf50b
Sig Lange <sig.lange@gmail.com>
2025-06-19 06:33:51 +0000

    sort jobs by jobid

--- ui/index.go
+++ ui/index.go
@@ -3,6 +3,8 @@ package ui
 import (
 	"log/slog"
 	"net/http"
+	"slices"
+	"strings"
 
 	"github.com/sigmonsays/jobd/job"
 )
@@ -13,9 +15,18 @@ type IndexPage struct {
 }
 
 func (me *Ui) Index(w http.ResponseWriter, r *http.Request) {
+
+	// duplicate jobs array for sorting
+	jobs := make([]*job.JobSpec, 0)
+	for _, job := range me.Context.AppConfig.Jobs {
+		jobs = append(jobs, job)
+	}
+	slices.SortFunc(jobs, func(a, b *job.JobSpec) int {
+		return strings.Compare(a.JobId, b.JobId)
+	})
 	data := &IndexPage{
 		Title: "jobd",
-		Jobs:  me.Context.AppConfig.Jobs,
+		Jobs:  jobs,
 	}
 
 	tmpl, err := me.getTemplates()
